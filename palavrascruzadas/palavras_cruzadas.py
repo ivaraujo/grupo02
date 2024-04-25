943f80a8c3bb0ce07096e1f297413c5ab2341ba9
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2024-04-25 16:43:26 -0300

    Update

--- palavrascruzadas/palavras_cruzadas.py
+++ palavrascruzadas/palavras_cruzadas.py
@@ -1,6 +1,6 @@
 import random
 
-
+cPalavra = [[" "," "," "," "],[" "," "," "," "],[" "," "," "," "],[" "," "," "," "]]
 
 #FUNÇÕES
 
@@ -17,14 +17,27 @@ def verificaPalavra(vetor,n_sorteado):
     cenario2 = [[" ","C"," "," "],["M","A","Ç","Ã"],[" ","J"," "," "],["V","A","S","O"]]
     cenario3 = [[" ","C"," "," "],["V","A","S","O"],[" ","J"," "," "],["M","A","Ç","Ã"]]
     cenario4 = [[" ","V"," ","M"],["C","A","J","A"],[" ","S"," ","Ç"],[" ","O"," ","Ã"]]
+    novoVetor = []
+    for x in range(4):
+        for y in range(4):
+            if(vetor[x][y] == cenario1[x][y]):
+                novoVetor[x][y] = cenario1[x][y]
+                print(novoVetor)
+    return novoVetor
 
-def jogar(n_sort):
+def jogar(n_sort,cPalav):
     while True:
+        print("_"*13)
+        for i in range(4): #EXIBIR CENÁRIO
+            for j in range(4):
+                print("|",cPalav[i][j], end="")                        
+            print("|")
+            print("¯"*13)
         print("3º palavra:")
         n_palavra = int(input("Nº da palavra: "))
         palavra = input("Digite a palavra: ")
         vet_palavra = list(palavra)
-        verificaPalavra(vet_palavra,n_sort)
+        cPalav = verificaPalavra(vet_palavra,n_sort)
 '''
 def imprimir(cenario):
     for i in range(4): #EXIBIR CENÁRIO
@@ -49,7 +62,7 @@ while True:
     opcao = int(input("--> "))
     if (opcao == 1):
         #imprimir(sorteado)
-        jogar(sorteado)
+        jogar(sorteado,cPalavra)
     elif (opcao == 2):
         break
     else:
