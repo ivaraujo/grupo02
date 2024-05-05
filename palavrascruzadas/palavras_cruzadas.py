0bc34d2bd8189c4850acf05be942b451db98bd28
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2024-05-05 08:38:46 -0300

    Correção do laço

--- palavrascruzadas/palavras_cruzadas.py
+++ palavrascruzadas/palavras_cruzadas.py
@@ -9,14 +9,14 @@ def sortearCenario(): #SORTEADOR DE CENÁRIO
     print(f"Cenário: {n}")
     return n
 
-def verificaPalavra(vetor,n_sorteado):
+def verificaPalavra(vetor,n_sorteado,l):
     # CENÁRIOS
     cenario1 = [[" ","M"," ","V"],["C","A","J","A"],[" ","Ç"," ","S"],[" ","Ã"," ","O"]]
     cenario2 = [[" ","C"," "," "],["M","A","Ç","Ã"],[" ","J"," "," "],["V","A","S","O"]]
     cenario3 = [[" ","C"," "," "],["V","A","S","O"],[" ","J"," "," "],["M","A","Ç","Ã"]]
     cenario4 = [[" ","V"," ","M"],["C","A","J","A"],[" ","S"," ","Ç"],[" ","O"," ","Ã"]]
     novoVetor = [[" "," "," "," "],[" "," "," "," "],[" "," "," "," "],[" "," "," "," "]]
-    v = 0
+    v = 0    
     if(n_sorteado == 1):
         print(cenario1)
         for x in range(4):
@@ -26,7 +26,8 @@ def verificaPalavra(vetor,n_sorteado):
                     #print(vetor[v])
                     novoVetor[x][y] = vetor[v]
                     v+=1
-            print()
+                    l+=1
+                    break
     if(n_sorteado == 2):
         print(cenario2)
         for x in range(4):
@@ -36,7 +37,8 @@ def verificaPalavra(vetor,n_sorteado):
                     #print(vetor[v])
                     novoVetor[x][y] = vetor[v]
                     v+=1
-            print()
+                    l+=1
+                    break
     if(n_sorteado == 3):
         print(cenario3)
         for x in range(4):
@@ -46,7 +48,8 @@ def verificaPalavra(vetor,n_sorteado):
                     #print(vetor[v])
                     novoVetor[x][y] = vetor[v]
                     v+=1
-            print()
+                    l+=1
+                    break
     if(n_sorteado == 4):
         print(cenario4)
         for x in range(4):
@@ -56,39 +59,27 @@ def verificaPalavra(vetor,n_sorteado):
                     #print(vetor[v])
                     novoVetor[x][y] = vetor[v]
                     v+=1
-            print()
+                    l+=1
+                    break
     print(novoVetor)
     return novoVetor
 
 def jogar(n_sort,cPalav):
-    while True:
+    loop = 0
+    while (loop < 11):
+        print("Loop:",loop)
         print("_"*13)
         for i in range(4): #EXIBIR JOGO ATUAL
             for j in range(4):
                 print("|",cPalav[i][j], end="")                        
             print("|")
             print("¯"*13)
-        print("3º palavra:")
-        n_palavra = int(input("Nº da palavra: "))
+        #print("3º palavra:")
+        #n_palavra = int(input("Nº da palavra: "))
         palavra = str(input("Digite a palavra: "))
-        #palavra = palavra.upper()
         vet_palavra = list(palavra.upper())
         print(vet_palavra)
-        #cPalav = 
-        verificaPalavra(vet_palavra,n_sort)
-'''
-def imprimir(cenario):
-    for i in range(4): #EXIBIR CENÁRIO
-        for j in range(4):
-            if (cenario == 1):
-                print(cenario1[i][j], end=" ")
-            if (cenario == 2):
-                print(cenario2[i][j], end=" ")
-            if (cenario == 3):
-                print(cenario3[i][j], end=" ")
-            if (cenario == 4):
-                print(cenario4[i][j], end=" ")        
-        print()'''
+        cPalav, loop = verificaPalavra(vet_palavra,n_sort,loop)
     
 # MENU
 
@@ -99,7 +90,7 @@ while True:
     print("2 - Sair")
     opcao = int(input("--> "))
     if (opcao == 1):        
-        jogar(sorteado,cPalavra)
+       jogar(sorteado,cPalavra)
     elif (opcao == 2):
         break
     else:
